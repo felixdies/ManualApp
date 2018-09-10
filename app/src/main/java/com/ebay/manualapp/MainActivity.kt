83f82216bb784b90b2e74b8d8860fda09bba498d
felixdies <felixdies@daum.net>
2018-09-10 11:48:26 +0900

    tvHello

--- app/src/main/java/com/ebay/manualapp/MainActivity.kt
+++ app/src/main/java/com/ebay/manualapp/MainActivity.kt
@@ -4,9 +4,13 @@ import android.app.Activity
 import android.os.Bundle
 
 class MainActivity : Activity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userItems: MutableList<Item> = mutableListOf()
+
     }
+
+    class Item (val name: String, val img: String, val remain: Int)
 }
