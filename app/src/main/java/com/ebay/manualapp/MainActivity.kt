287d8a993a83a80c64968c4e9b525fef7935c453
felixdies <felixdies@daum.net>
2018-09-10 23:10:12 +0900

    main listview

--- app/src/main/java/com/ebay/manualapp/MainActivity.kt
+++ app/src/main/java/com/ebay/manualapp/MainActivity.kt
@@ -3,39 +3,77 @@ package com.ebay.manualapp
 import android.app.Activity
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View.MeasureSpec
+import android.opengl.ETC1.getWidth
+import android.widget.ListView
+
+
 
 class MainActivity : Activity() {
+    var userItems = ArrayList<Item>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val userItems: MutableList<Item> = mutableListOf()
-        userItems.add(Item("삼성 블루스카이\n공기청정기", "item1.png", 326, 365))
-        userItems.add(Item("다이슨 V10\n앱솔루트 플러스", "item2.png", 26, 180))
-    }
-
-    inner class ItemsAdapter: BaseAdapter {
-        private var context: Context? = null
-        private var itemList = ArrayList<Item>()
-
-        constructor(context: Context, itemList: ArrayList<Item>) : super() {
-            this.itemList = itemList
-            this.context = context
+        // ListView 가 ScrollView 와 함께 Scroll 되도록 함
+        lvItems.setOnTouchListener { v, event ->
+            v.parent.requestDisallowInterceptTouchEvent(false)
+            return@setOnTouchListener false
         }
 
+        addItemList(Item("삼성 블루스카이\n공기청정기", "item1.png", 326, 365))
+        addItemList(Item("다이슨 V10\n앱솔루트 플러스", "item2.png", 26, 180))
+        addItemList(Item("필립스 소닉케어\nHX6711", "item3.png", 143, 180))
+        addItemList(Item("삼성 레이저프린터\nSL-M2027", "item4.png", 200, 365))
+
+        /*
+        lvNotes.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, position, id ->
+            Toast.makeText(this, "Click on " + listNotes[position].title, Toast.LENGTH_SHORT).show()
+        }
+        */
+    }
+
+    fun addItemList(item: Item) {
+        userItems.add(item)
+        var itemsAdapter = ItemsAdapter(this, userItems)
+        lvItems.adapter = itemsAdapter
+        setListViewHeightBasedOnChildren(lvItems)
+    }
+
+    inner class ItemsAdapter(context: Context, itemList: ArrayList<Item>): BaseAdapter() {
+        var ctx = context
+        var items = itemList
+
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
-            val view: View? = null
+            val view: View?
             val vh: ViewHolder
+
+            if (convertView == null) {
+                view = layoutInflater.inflate(R.layout.list_item, parent, false)
+                vh = ViewHolder(view)
+                view.tag = vh
+                Log.i("JSA", "set Tag for ViewHolder, position: " + position)
+            } else {
+                view = convertView
+                vh = view.tag as ViewHolder
+            }
+
+            vh.tvItemName.text = items[position].name
+            vh.tvRemain.text = "-" + items[position].remain.toString() + "일"
 
             return view
         }
 
         override fun getItem(position: Int): Any {
-            return itemList[position]
+            return items[position]
         }
 
         override fun getItemId(position: Int): Long {
@@ -43,19 +81,43 @@ class MainActivity : Activity() {
         }
 
         override fun getCount(): Int {
-            return itemList.size
+            return items.size
         }
     }
 
     private class ViewHolder(view: View?) {
-        val tvTitle: TextView
-        val tvContent: TextView
+        val imgItem: ImageView
+        val tvItemName: TextView
+        val tvRemain: TextView
 
         init {
-            this.tvTitle = view?.findViewById(R.id.tvTitle) as TextView
-            this.tvContent = view?.findViewById(R.id.tvContent) as TextView
+            this.imgItem = view?.findViewById(R.id.imgItem) as ImageView
+            this.tvItemName= view?.findViewById(R.id.tvItemName) as TextView
+            this.tvRemain= view?.findViewById(R.id.tvRemain) as TextView
         }
     }
 
-    class Item (val name: String, val img: String, val remain: Int, val total)
+    /**** Method for Setting the Height of the ListView dynamically.
+     * Hack to fix the issue of not showing all the items of the ListView
+     * when placed inside a ScrollView   */
+    fun setListViewHeightBasedOnChildren(listView: ListView) {
+        val listAdapter = listView.getAdapter() ?: return
+
+        val desiredWidth = MeasureSpec.makeMeasureSpec(listView.getWidth(), MeasureSpec.UNSPECIFIED)
+        var totalHeight = 0
+        var view: View? = null
+        for (i in 0 until listAdapter.getCount()) {
+            view = listAdapter.getView(i, view, listView)
+            if (i == 0)
+                view!!.layoutParams = ViewGroup.LayoutParams(desiredWidth, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+            view!!.measure(desiredWidth, MeasureSpec.UNSPECIFIED)
+            totalHeight += view.measuredHeight
+        }
+        val params = listView.getLayoutParams()
+        params.height = totalHeight + listView.getDividerHeight() * (listAdapter.getCount() - 1)
+        listView.setLayoutParams(params)
+    }
+
+    class Item (val name: String, val img: String, val remain: Int, val total: Int)
 }
